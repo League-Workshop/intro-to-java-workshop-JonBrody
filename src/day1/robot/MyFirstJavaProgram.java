4bbf58abec2f1e896ff26d1be08a07118d883077
JonBrody <league@WTS-iMac-11.attlocal.net>
2017-08-22 14:53:45 -0700

    boba feet rules
    boba can beat up luke

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,8 +7,57 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot BobaFett=new Robot("batman");
+BobaFett.setSpeed(10);
+BobaFett.penDown();
+for (int i = 0; i < 4; i++) {
 	
+}
+BobaFett.move(200);
+BobaFett.turn(90);
+BobaFett.move(200);
+BobaFett.turn(-90);
+		BobaFett.move(-200);
+		BobaFett.turn(90);
+		BobaFett.move(-200);
+		BobaFett.move(200);
+		BobaFett.turn(90);
+		BobaFett.move(200);
+		BobaFett.turn(-90);
+				BobaFett.move(-200);
+				BobaFett.turn(90);
+				BobaFett.move(-200);
+				BobaFett.move(200);
+				BobaFett.turn(90);
+				BobaFett.move(200);
+				BobaFett.turn(-90);
+						BobaFett.move(-200);
+						BobaFett.turn(90);
+						BobaFett.move(-200);
+						BobaFett.move(200);
+						BobaFett.turn(90);
+						BobaFett.move(200);
+						BobaFett.turn(-90);
+								BobaFett.move(-200);
+								BobaFett.turn(90);
+								BobaFett.move(-200);
+								BobaFett.move(200);
+								BobaFett.turn(90);
+								BobaFett.move(200);
+								BobaFett.turn(-90);
+										BobaFett.move(-200);
+										BobaFett.turn(90);
+										BobaFett.move(-200);
+										BobaFett.move(200);
+										BobaFett.turn(90);
+										BobaFett.move(200);
+										BobaFett.turn(-90);
+												BobaFett.move(-200);
+												BobaFett.turn(90);
+												BobaFett.move(-200);
+												
+												
+		
 		
 		
 	}
